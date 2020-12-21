ddce77316aa508c6a4ebc483158dd44d57bfd82a
MA-Wenhui <13901820382@163.com>
2020-12-21 11:30:36 +0800

    finish monte carlo prediction

--- frozen_value_iteration.py
+++ frozen_value_iteration.py
@@ -8,37 +8,38 @@ env = env.unwrapped
 print(env.observation_space)
 print(env.action_space)
 
-def value_iteration(env, gamma = 1.0):
+
+def value_iteration(env, gamma=1.0):
     value_table = np.zeros(env.observation_space.n)
-    #print(value_table)
+    # print(value_table)
     no_of_iterations = 100000
     threshold = 1e-20
     for i in range(no_of_iterations):
         updated_value_table = np.copy(value_table)
         for state in range(env.observation_space.n):
             Q_value = []
-            for action in range(env.action_space.n):   # for every possible action
-                next_state_rewards = []  
-                for next_sr in env.P[state][action]:   
-                    trans_prob, next_state, reward_prob,_ = next_sr
-                    next_state_rewards.append((trans_prob*(reward_prob+gamma*updated_value_table[next_state])))
+            for action in range(env.action_space.n):  # for every possible action
+                next_state_rewards = []
+                for next_sr in env.P[state][action]:
+                    trans_prob, next_state, reward_prob, _ = next_sr
+                    next_state_rewards.append((trans_prob * (reward_prob + gamma * updated_value_table[next_state])))
                 Q_value.append(np.sum(next_state_rewards))
             value_table[state] = max(Q_value)
-        if(np.sum(np.fabs(updated_value_table-value_table))<=threshold):
-            print ('Value-iteration converged at iteration# %d.' %(i+1))
+        if np.sum(np.fabs(updated_value_table - value_table)) <= threshold:
+            print('Value-iteration converged at iteration# %d.' % (i + 1))
             break
-        #print(value_table)
+        # print(value_table)
     return value_table
 
 
-def extract_policy(value_table, gamma = 1.0):
+def extract_policy(value_table, gamma=1.0):
     policy = np.zeros(env.observation_space.n)
     for state in range(env.observation_space.n):
         Q_table = np.zeros(env.action_space.n)
         for action in range(env.action_space.n):
             for next_sr in env.P[state][action]:
-                trans_prob, next_state, reward_prob,_ = next_sr
-                Q_table[action] +=(trans_prob*(reward_prob+gamma*value_table[next_state]))
+                trans_prob, next_state, reward_prob, _ = next_sr
+                Q_table[action] += (trans_prob * (reward_prob + gamma * value_table[next_state]))
         policy[state] = np.argmax(Q_table)
     return policy
 
---
+++ fv_mc_blackjack.py
@@ -0,0 +1,81 @@
+import gym
+from matplotlib import pyplot
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from collections import defaultdict
+from functools import partial
+
+plt.style.use('ggplot')
+
+env = gym.make('Blackjack-v0')
+
+
+def sample_policy(observation):
+    score, dealer_score, usable_ace = observation
+    return 0 if score >= 20 else 1
+
+
+def generate_episode(env):
+    states, actions, rewards = [], [], []
+    observation = env.reset()
+    while True:
+        states.append(observation)
+        action = sample_policy(observation)
+        actions.append(action)
+        observation, reward, done, info = env.step(action)
+        rewards.append(reward)
+        if done:
+            break
+    return states, actions, rewards
+
+
+player_sum = np.arange(12, 21 + 1)
+dealer_show = np.arange(1, 10 + 1)
+usable_ace = np.array([False, True])
+
+X, Y = np.meshgrid(player_sum, dealer_show)
+state_values = np.zeros((len(player_sum),
+                         len(dealer_show),
+                         len(usable_ace)))
+
+fig, axes = pyplot.subplots(nrows=2, figsize=(5, 8), subplot_kw={'projection': '3d'})
+for ax in axes:
+    ax.set_zlim(-1, 1)
+    ax.set_ylabel('player sum')
+    ax.set_xlabel('dealer showing')
+    ax.set_zlabel('state-value')
+
+
+def plot_blackjack(V, ax1, ax2):
+    for i, player in enumerate(player_sum):
+        for j, dealer in enumerate(dealer_show):
+            for k, ace in enumerate(usable_ace):
+                state_values[i, j, k] = V[player, dealer, ace]
+
+    ax1.scatter(X, Y, state_values[:, :, 0])
+    ax2.scatter(X, Y, state_values[:, :, 1])
+
+
+value_table = defaultdict(float)
+
+
+def first_visit_mc_prediction(env, n_episodes):
+    N = defaultdict(int)
+    for _ in range(n_episodes):
+        states, _, rewards = generate_episode(env)
+        returns = 0
+        for t in range(len(states) - 1, -1, -1):
+            R = rewards[t]
+            S = states[t]
+            returns += R
+            if S not in states[:t]:
+                N[S] += 1
+                value_table[S] += (returns - value_table[S]) / N[S]
+        axes[0].cla()
+        axes[1].cla()
+        plot_blackjack(value_table, axes[0], axes[1])
+        plt.pause(0.001)
+
+
+first_visit_mc_prediction(env, 100000)
